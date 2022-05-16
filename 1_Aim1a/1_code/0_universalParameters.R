523465cfd77d52371ce8ddc4b114bc5f6d3681db
Nikki Freeman <nlbfreeman@gmail.com>
2022-05-16 11:51:36 -0400

    Added amputation rates to universal parameters

--- 1_Aim1a/1_code/0_universalParameters.R
+++ 1_Aim1a/1_code/0_universalParameters.R
@@ -1,6 +1,6 @@
 # 0_universalParameters.R
 # Author: Nikki Freeman
-# Last modified: 25 April 2022
+# Last modified: 9 May 2022
 
 # Preamble ---------------------------------------------------------------------
 # This file contains the universal simulation parameters used for all of the 
@@ -11,6 +11,7 @@ N <- c(550, 600, 650, 700, 750) # Study size
 C_L <- 0.15 # LTFU rate
 mort2yr <- seq(from = 0.15, to = 0.45, by = 0.05)
 woundRecur <- seq(from = 0.25, to = 0.55, by = 0.05)
+amp2yr <- c(0.20, 0.25)
 L <- 1000 # Number of simulations for power calc
 responseProbRange <- list(p1 = c(0.25, 0.35),
                           p2 = c(0.25, 0.35),
@@ -22,3 +23,10 @@ responseProbRange <- list(p1 = c(0.25, 0.35),
 parameterGrid <- expand.grid(mort2yr0 = mort2yr, mort2yr1 = mort2yr, woundRecur0 = woundRecur, woundRecur1 = woundRecur, C_L = C_L) %>%
   # Remove the rows where mortality and wound recurrence are the same in both arms
   filter(!(mort2yr1 == mort2yr0 & woundRecur1 == woundRecur0)) 
+
+# Create a grid of 2-year mort, 2-year wound recurrence, and 2-year amputation values
+# to evaluate over
+parameterGrid_withAmp <- expand.grid(mort2yr0 = mort2yr, mort2yr1 = mort2yr,
+                                     woundRecur0 = woundRecur, woundRecur1 = woundRecur,
+                                     amp2yr0 = amp2yr, amp2yr1 = amp2yr) %>%
+  filter(!(mort2yr1 == mort2yr0 & woundRecur1 == woundRecur0 & amp2yr1 == amp2yr0))
